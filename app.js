26d44dc4bee286b924edaafda951f44b1dd1b25f
MakAcp <nihar.kalsekar@gmail.com>
2022-04-14 13:19:16 -0400

    added twilio functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const res = require('express/lib/response');
+const sendmail = require("@sendgrid/mail");
 const mongoose = require('mongoose');
 const app= express();
 const bodyparser=require('body-parser');
@@ -8,9 +9,7 @@ require('dotenv/config');
 //Middleware normally used for authenticaion eg app.use(auth)
 app.use(bodyparser.json());
 const postsRoute= require('./routes/posts')
-
 app.use('/posts',postsRoute);
-
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
 const db = mongoose.connection
 db.once('open', _ => {
@@ -28,6 +27,18 @@ app.get('/',(req,res) => {
 
 });
 
+
+const API_KEY= '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'
+sendmail.setApiKey(API_KEY);
+const message={
+    to: "2017.nihar.kalsekar@ves.ac.in",
+    from: "nihar.kalsekar@gmail.com",
+    subject:"seding u love",
+    text:"lol trolled",
+    html:'<h1>lol trolled</h1>',
+};
+sendmail.send(message)
+.then(response=> console.log('Email sent...'))
 
 
 
